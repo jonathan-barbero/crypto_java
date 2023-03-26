22bd69d60ed6d299e46dc478fa8b4e7999e1aae9
jonathan-barbero <jonathan.barbero@gmail.com>
2023-03-26 09:36:08 -0300

    pom.xml

---
+++ src/main/java/ar/com/job/crypto/jep356/JEP356.java
@@ -0,0 +1,39 @@
+package ar.com.job.crypto.jep356;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+import java.util.stream.Stream;
+
+public class JEP356 {
+
+    public static void getAllPRNGs() {
+
+        Stream<RandomGeneratorFactory<RandomGenerator>> allPRNGs = RandomGeneratorFactory.all();
+        allPRNGs.map(prng ->
+                prng.name() + " [ Group: " + prng.group() + "; "
+                        + (prng.isArbitrarilyJumpable() ? " arbitrary-jump" : "")
+                        + (prng.isHardware() ? " hardware" : "") + (prng.isJumpable() ? " jump" : "")
+                        + (prng.isLeapable() ? " leap" : "") + (prng.isSplittable() ? " split" : "")
+                        + (prng.isStatistical() ? " statistical" : "") + (prng.isStochastic() ? " stochastic" : "")
+                        + (prng.isStreamable() ? " stream" : "") + "; noOfBits: " + prng.stateBits()
+                        + "]"
+        ).sorted().forEach(System.out::println);
+
+        System.out.println("\n*****\n");
+
+        RandomGenerator prng1 = RandomGeneratorFactory.
+                of("Random").create(45);
+        System.out.println("prng1 - " + prng1.getClass());
+
+        RandomGenerator prng2 = new Random(45);
+        System.out.println("prng2 - " + prng2.getClass());
+
+        RandomGenerator prng3 = RandomGeneratorFactory.
+                getDefault().create(45);
+        System.out.println("prng3 - " + prng3.getClass());
+
+        RandomGenerator prng4 = RandomGenerator.getDefault();
+        System.out.println("prng4 - " + prng4.getClass());
+    }
+}
---
+++ src/test/java/ar/com/job/crypto/jep356/JEP356Test.java
@@ -0,0 +1,11 @@
+package ar.com.job.crypto.jep356;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JEP356Test {
+
+    @org.junit.jupiter.api.Test
+    void getAllPRNGs() {
+        JEP356.getAllPRNGs();
+    }
+}
